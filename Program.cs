3b01e4947cf6cc51590aebc349285d055f46016f
Aryaman Shrestha <87716416+aryamanstha@users.noreply.github.com>
2023-05-02 16:38:53 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void SaveFile(List<string> strings)
+        {
+            StreamWriter sw = File.AppendText("demo.txt");
+            foreach(string s in strings)
+            {
+                sw.WriteLine(s);
+            }
+            sw.Flush();
+            sw.Close();
+        }
+        public static void ReadFile()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("demo.txt");
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("The file cannot be read.");
+            }
+            
+        }
+        public static void FileSave()
+        {
+            List<string> list = new List<string>();
+             top:
+            Console.WriteLine("Enter the string to save:");
+            string value = Console.ReadLine();
+            list.Add(value);
+            SaveFile(list);
+            Console.WriteLine("Do you want to add more?(y/n)");
+            string ch = Console.ReadLine();
+            if (ch == "Y" || ch == "y")
+            {
+                goto top;
+            }
+            Console.WriteLine("The Contents in file are:");
+            ReadFile();
+        }
+        public static void Main(string[] args)
+        {
+            FileSave();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    public class StudentCollection
+    {
+        public class Student
+        {
+            public int sid { get; set; }
+            public string sname { get; set; } = string.Empty;
+            public int sage { get; set; }
+            public string saddress { get; set; } = string.Empty;
+            public int sclass { get; set; }
+
+        }
+        public static void AddToList()
+        {
+            var stdlist = new List<Student>
+        {
+            new Student{sid=1,sname="Ram",saddress="Kathmandu",sage=10,sclass=5 },
+            new Student{sid=2,sname="Shyam",saddress="Pokhara",sage=10,sclass=5 },
+            new Student{sid=3,sname="Hari",saddress="Birjung",sage=10,sclass=5 },
+            new Student{sid=4,sname="Gita",saddress="Kathmandu",sage=10,sclass=5 },
+            new Student{sid=5,sname="Sita",saddress="Jomsom",sage=10,sclass=5 }
+        };
+        }
+        public static void DisplayList(IList<Student> stdlist)
+        {
+            var std = from s in stdlist
+                      where s.saddress == "Kathmandu"
+                      select s;
+                    
+                    
+                    
+        }
+    }
+
+}
